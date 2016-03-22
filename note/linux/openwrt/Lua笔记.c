a9bcceb394bbd5789f0415fcb8adc44a537b77f2
code <504136046@qq.com>
2016-03-22 20:41:31 +0800

    lua笔记更新

--- note/linux/openwrt/Lua笔记.c
+++ note/linux/openwrt/Lua笔记.c
@@ -5,8 +5,11 @@
         and break do else elseif end false for function if in local nil not
         or repeat return then true until while 
 
-3. lua的变量不需要声明，变量不等于nil时，即表示存在,变量默认总是全局的
-   lua的变量不需要定义类型，只需要赋值就能使用
+3. lua的变量有三种：全局变量、局部变量、表中的域
+   函数外的变量默认为全局变量，除非用local声明，函数内变量与函数参数默认为局部变量
+   lua可以对多个变量同时赋值，变量列表和值列表的各个元素用逗号分开
+   lua是首先计算右边所有的值，然后再执行赋值操作，所以 x,y = y,x 可以实现交换变量值
+   当变量个数和值个数不一致时，lua以变量个数为基础，采取少补nil，多忽略的原则
 
 4. lua有8个基本数据类型：
         nil     - 只有1个值nil，表示无效值（条件表达式中相当于false）
@@ -25,4 +28,21 @@
         table   - 表，也可以理解为数组，数组索引可以是数字或字符串
                   不同于C语言把0作为数组的初始索引，lua里表的默认初始索引是1
                   表不会固定长度大小，而是随着数据添加自动增长
+
+5. lua提供3种循环处理方式：
+    while循环 - 语法格式 -  while(条件) do 执行体   end
+              - 备注     -  类C,略
+    for循环又细分为：数值for循环 - 语法格式 -   for var=exp1,exp2,exp3 do 执行体    end
+                                 - 备注     -   var从exp1变化到exp2,间隔exp3（可选，缺省值1）
+                                                for的3个exp在循环开始前一次性求值，以后不再进行求值
+                     泛型for循环 - 语法格式 -   for i,v in ipairs(a) do print(v)    end
+                                 - 备注     -   i是数组索引值，v是对应索引值的数组元素值，ipais是lua提供的一个迭代函数，用来迭代数组
+    repeat...until循环 - 语法格式 - repeat  执行体  while(条件)
+                       - 备注     - 类C，略
+    
+6. lua流程控制语句 if...else
+   lua中0为true，这点跟C不同，if语句要注意then这个关键词不能丢
+
+7. lua中函数默认为全局函数，头部加local表示为局部函数
+   lua函数可以返回多个值，以逗号隔开
 
