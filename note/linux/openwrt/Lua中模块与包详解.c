5dc793993c31438ba8658576495b160b2024c9bd
code <504136046@qq.com>
2016-03-27 15:46:12 +0800

    lua笔记

--- note/linux/openwrt/Lua中模块与包详解.c
+++ note/linux/openwrt/Lua中模块与包详解.c
@@ -12,7 +12,7 @@
             于是当我们直接调用模块名test时，其实就是在调用_G["test"]的内容M
         问题：  实际编程中，return M语句的省略似乎并没有产生错误提示
         
-        对于使用5.2版本一下的lua，模块文件还可以使用以下定义方法：
+        对于使用5.2版本以下的lua，模块文件还可以使用以下定义方法：
         module(...,package.seeall)
         function fun()
             print("hello")
@@ -25,4 +25,12 @@
                     ...
                     return M
         问题：  module函数5.3之后被彻底抛弃，官方给出原因是会污染全局环境变量
-lua通过一个require的函数来加载模块
+
+2. lua通过一个require的函数来加载模块，require函数定义如下:
+	require("模块名")
+	备注：	因为模块有可能是lua库或c库，所以模块名不能包含扩展名;
+			该函数会返回一个由模块函数组成的table
+
+	require函数的加载原则：
+		用模块名替换package.path中的每个“？”，然后检查是否存在这样一个文件;
+		如果找不到，接着在package.cpath中找
