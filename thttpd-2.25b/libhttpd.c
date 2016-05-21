21a8b66845481525cff58677d664bf22a7f064a2
code <504136046@qq.com>
2016-05-21 10:49:04 +0800

    删除libubox

--- thttpd-2.25b/libhttpd.c
+++ thttpd-2.25b/libhttpd.c
@@ -1221,19 +1221,19 @@ hexit( char c )
 */
 static void
 strdecode( char* to, char* from )
-    {
+{
     for ( ; *from != '\0'; ++to, ++from )
 	{
-	if ( from[0] == '%' && isxdigit( from[1] ) && isxdigit( from[2] ) )
-	    {
-	    *to = hexit( from[1] ) * 16 + hexit( from[2] );
-	    from += 2;
-	    }
-	else
-	    *to = *from;
+		if ( from[0] == '%' && isxdigit( from[1] ) && isxdigit( from[2] ) )
+		{
+		    *to = hexit( from[1] ) * 16 + hexit( from[2] );
+		    from += 2;
+		}
+		else
+		    *to = *from;
 	}
     *to = '\0';
-    }
+}
 
 
 #ifdef GENERATE_INDEXES
