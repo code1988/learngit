fb7557e02b9bdd43b9229d61d79e0f8df5606b8d
code <504136046@qq.com>
2016-10-08 11:48:48 +0800

    更新笔记

--- note/linux/APUE系列笔记/APUE线程笔记1.c
+++ note/linux/APUE系列笔记/APUE线程笔记1.c
@@ -80,15 +80,56 @@
      *
      * 备注 : (glibc2.19 + kernel3.16.36环境) 本函数只能获取通过pthread_attr_setstack设置的自定义的线程栈地址和栈大小属性,
      *        如果没有设置过自定义值，本函数返回空值。
-     *        本函数的这个特性APUE并未阐明，网上的资料大都是相互抄袭，人云亦云
+     *        本函数的这个特性APUE并未阐明，网上的资料大都是相互抄袭，人云亦云。
+     *        这里，我只能说，本函数的这个特性至少是跟glibc和kernel的版本相关的
      */
     
     /* API  : int pthread_attr_setstack(pthread_attr_t *attr,void *stackaddr,size_t stacksize)
      * 描述 : 自定义线程属性对象中的栈地址和栈大小属性
      * @attr        - 指向线程属性对象
-     * @stackaddr   - 用于设置自定义的线程栈地址
-     * @stacksize   - 用于设置自定义的线程栈大小
+     * @stackaddr   - 用于设置自定义的线程栈地址(通常要保证page边界对齐)，该地址将会作为线程栈的最低可寻址地址
+     * @stacksize   - 用于设置自定义的线程栈大小(通常要保证page大小的整数倍)
      *
+     * 备注 : 自定义线程栈的步骤：
+     *                          [1]. 调用malloc/mmap系列函数从堆中分配空间
+     *                          [2]. 调用本函数改变新建线程的栈位置
+     *                          [3]. 创建线程
+     */
+
+    /* API  : int pthread_attr_getstacksize(pthread_attr_t *attr,size_t *stacksize) 
+     * 描述 : 获取线程属性对象中的栈大小属性
+     * @attr        - 指向线程属性对象
+     * @stacksize   - 用于存放线程栈大小
+     *
+     * 备注 : 不同于pthread_attr_getstack，如果没有设置过自定义值，本函数返回缺省的栈大小(通常就是8M);如果设置了自定义值，本函数返回自定义值
+     */
+
+    /* API  : int pthread_attr_setstacksize(pthread_attr_t *attr,size_t stacksize)
+     * 描述 : 设置线程属性对象中的栈大小属性
+     * @attr        - 指向线程属性对象
+     * @stacksize   - 用于设置自定义的线程栈大小(不能小于PTHREAD_STACK_MIN，并且通常要保证STACK_ALIGN的整数倍)
+     *
+     * 备注 : 相比pthread_attr_setstack，本函数适用场景：即希望改变默认的栈大小，又不想自己处理线程栈的分配问题
+     */
+
+    /* API  : int pthread_attr_getguardsize(pthread_attr_t *attr,size_t *guardsize)
+     * 描述 : 获取线程属性对象中的栈末尾警戒缓冲区大小属性
+     * @attr        - 指向线程属性对象
+     * @guardsize   - 用于存放栈末尾警戒缓冲区大小(其缺省值一般是page大小)
+     *
+     * 备注 : 栈末尾警戒缓冲区用于避免栈溢出，具体的原理详见另一篇栈相关的文档
+     *        APUE中写道，"if we change the stackaddr thread attribute, the system assumes that we will be
+     *        managing our own stacks and disables stack guard buffers, just as if we had set the
+     *        guardsize thread attribute to 0"
+     *        在本文的测试环境Debian 8.3(glibc2.19 + kernel3.16.36)中，结果并非这样:在调用了pthread_attr_setstack之后，guardsize的值仍旧是缺省的page大小
+     */
+
+    /* API  : int pthread_attr_setguardsize(pthread_attr_t *attr,size_t guardsize)
+     * 描述 : 设置线程属性对象中的栈末尾警戒缓冲区大小属性
+     * @attr        - 指向线程属性对象
+     * @guardsize   - 用于设置警戒缓冲区大小
+     *
+     * 备注 : 虽然不被建议这么做，但是可以将guardsize的值设为0，意味着不再提供警戒缓冲区机制
      */
     
 5. 线程创建
@@ -102,3 +143,96 @@
      * 备注 : 同进程创建使用的fork调用类似，线程创建时并不能确保哪个线程先运行
      */ 
     
+附录：测试代码
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+pthread_t ntid;
+pthread_attr_t attr;
+
+void printids(const char *s)
+{
+    pid_t pid;
+    pthread_t tid;
+
+    pid = getpid();
+    tid = pthread_self();
+
+    printf("%s pid = %lu,tid = %lu(0x%lx)\n",s,(unsigned long)pid,(unsigned long)tid,(unsigned long)tid);
+}
+
+void *thr_fn(void *arg)
+{
+    printids("new thread:");
+
+    return (void *)2;
+}
+int main()
+{
+    int err;
+
+    if(pthread_attr_init(&attr)){
+        printf("init pthread attr error\n");
+        return -1;
+    }
+
+    int detachstate;
+    if(pthread_attr_getdetachstate(&attr,&detachstate)){
+        printf("get pthread's detach state error\n");
+        return -1;
+    }
+    printf("pthread's detach state = %s\n",detachstate == PTHREAD_CREATE_JOINABLE?"joinable":"detach");
+
+    size_t len = 1024 * 1000 * 4;
+    void *buf = malloc(len);
+    printf("malloc pthread stack addr = %p, len = %d\n",buf,len);
+    if(pthread_attr_setstack(&attr,buf,len)){
+        printf("set ptherad's stack attr error\n");
+        return -1;
+    }
+    printf("set pthread stack attr ok\n");
+
+    void *stackaddr;
+    size_t stacksize;
+    if(pthread_attr_getstack(&attr,&stackaddr,&stacksize)){
+        printf("get ptherad's stack attr error\n");
+        return -1;
+    }
+    printf("pthread's stack addr = %p,size = %d\n",stackaddr,stacksize);
+
+    if(pthread_attr_getstacksize(&attr,&stacksize)){
+        printf("get ptherad's stack size error\n");
+        return -1;
+    }
+    printf("pthread's stack size = %d\n",stacksize);
+
+    size_t guardsize;
+    if(pthread_attr_getguardsize(&attr,&guardsize)){
+        printf("get ptherad's stack size error\n");
+        return -1;
+    }
+    printf("pthread's guard size = %d\n",guardsize);
+
+    err = pthread_create(&ntid,NULL,thr_fn,NULL); 
+    if(err){
+        printf("create ptherad error = %d\n",err);
+        return -1;
+    }
+    printids("main thread:");
+    sleep(1);
+
+    if(pthread_attr_destroy(&attr)){
+        printf("destory ptherad attr error \n");
+        return -1;
+    }
+
+    void *state;
+    if(pthread_join(ntid,&state)){
+        printf("join ptherad error \n");
+        return -1;
+    }
+    printf("join pthread ok,state = %d\n",(unsigned int)state);
+
+    exit(0);
+}
