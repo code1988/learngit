c783b21d55d4e3be43782a5d2e20ee3393c3d96e
code <504136046@qq.com>
2018-01-04 03:05:13 +0800

    更新笔记

--- note/linux/APUE系列笔记/APUE线程同步.c
+++ note/linux/APUE系列笔记/APUE线程同步.c
@@ -66,4 +66,97 @@
     读写锁的使用规则：
             [1].当读写锁是写模式下上锁时，所有试图对这个锁上锁(不管是读上锁还是写上锁)的线程都会被阻塞；
             [2].当读写锁是读模式下上锁时，所有试图以读模式对它进行上锁的线程都可以上锁成功，但是试图以写模式对它进行上锁的线程都会被阻塞，直到所有线程释放了读锁
-            [2].当读写锁是读模式下上锁时，
+    类似于互斥量，使用读写锁之前，必须先对它进行初始化，初始化读写锁的方式有以下2种：
+        [1]. 对于静态分配的读写锁通常在定义时就可以直接设置为常量PTHREAD_RWLOCK_INITIALIZER
+        [2]. 对于动态分配的读写锁通常调用pthread_rwlock_init进行初始化
+
+    /* API  : int pthread_rwlock_init(pthread_rwlock_t *rwlock,pthread_rwlockattr_t *attr)
+     * 描述 : 初始化读写锁
+     * @rwlock  - 指向一个要初始化的读写锁
+     * @attr    - 用于设置读写锁的属性，NULL表示设置默认属性
+     */
+
+    /* API  : int pthread_rwlock_destroy(pthread_rwlock_t *rwlock)
+     * 描述 : 销毁读写锁
+     * @rwlock  - 指向一个要销毁的读写锁
+     *
+     * 备注 : 销毁之前必须确保没有线程在该读写锁上阻塞
+     *        销毁之前必须确保该读写锁没有被上锁
+     *        不需要销毁使用PTHREAD_RWLOCK_INITIALIZER初始化的静态分配的读写锁
+     */
+
+    读写锁POSIX标准中只定义了1个进程共享属性(类似于互斥量的进程共享属性)，
+
+    /* API  : int pthread_rwlock_rdlock(pthread_rwlock_t *rwlock)
+     * 描述 : 读模式下进行加锁
+     * @rwlock  - 指向一个要加读锁的读写锁
+     */
+
+    /* API  : int pthread_rwlock_wrlock(pthread_rwlock_t *rwlock)
+     * 描述 : 写模式下进行加锁
+     * @rwlock  - 指向一个要加写锁的读写锁
+     */
+
+    /* API  : int pthread_rwlock_unlock(pthread_rwlock_t *rwlock)
+     * 描述 : 对读写锁进行解锁(读锁和写锁通用)
+     * @rwlock  - 指向一个要解锁的读写锁
+     */
+
+    类似于互斥量，读写锁也有非阻塞方式和带超时时间的API，格式和用法基本都类似，这里不再展开分析
+    显然读写锁非常适合于对数据结构读的次数远大于写的场景
+
+3. 条件变量(pthread_cond_init)
+    条件变量需要配合互斥量一起使用，线程在改变条件变量之前必须先锁住互斥量。
+    类似于互斥量，使用条件变量之前，必须先对它进行初始化，初始化条件变量的方式有以下2种：
+        [1]. 对于静态分配的条件变量通常在定义时就可以直接设置为常量PTHREAD_COND_INITIALIZER
+        [2]. 对于动态分配的条件变量通常调用pthread_cond_init进行初始化
+    
+    /* API  : int pthread_cond_init(pthread_cond_t *cond,pthread_condattr_t *attr)
+     * 描述 : 初始化条件变量
+     * @cond    - 指向一个要初始化的条件变量
+     * @attr    - 用于设置条件变量的属性，NULL表示设置默认属性
+     */
+
+    /* API  : int pthread_cond_destroy(pthread_cond_t *cond)
+     * 描述 : 销毁条件变量
+     * @cond    - 指向一个要销毁的条件变量
+     *
+     * 备注 : 销毁之前必须确保没有线程在该条件变量上阻塞
+     *        不需要销毁使用PTHREAD_COND_INITIALIZER初始化的静态分配的条件变量
+     */
+
+    条件变量主要包含以下2个属性：
+        [1]. 进程共享属性(类似于互斥量)
+        [2]. 时钟属性: 用于控制pthread_cond_timedwait传入的超时参数采用的是哪个时钟，缺省值是CLOCK_REALTIME，也就是日历时间
+
+    /* API  : int pthread_cond_wait(pthread_cond_t *cond,pthread_mutex_t *mutex)
+     * 描述 : 等待条件变量为真
+     * @cond    - 指向一个要等待的条件变量
+     * @mutex   - 指向一个用来对条件变量进行保护的互斥量
+     *
+     * 备注 : 调用本函数前需要先锁住该互斥量，然后本函数在执行的过程中会自动把调用线程放到条件变量的等待队列中，并且解锁互斥量
+     *        当条件变量为真时，本函数在返回前会重新锁住互斥量
+     *        在本函数返回之后，需要重新检查条件(也就是条件变量作用的具体数据结构)是否可用(因为可能存在一些极端情况，比如多个线程同时在等待该条件变量，先等到的线程已经改掉了条件)
+     */
+
+    /* API  : int pthread_cond_timedwait(pthread_cond_t *cond,pthread_mutex_t *mutex,const struct timespec *tsptr)
+     * 描述 : 等待条件变量为真(带超时时间)
+     * @cond    - 指向一个要等待的条件变量
+     * @mutex   - 指向一个用来对条件变量进行保护的互斥量
+     * @tsptr   - 指向一个精确到纳秒的日历时间
+     *
+     * 备注 : 如果阻塞到设置的日历时间该条件变量还没有为真，直接返回 ETIMEDOUT
+     */
+
+    /* API  : int pthread_cond_signal(pthread_cond_t *cond)
+     * 描述 : 通知至少一个等待该条件的线程条件已经满足(即条件变量作用的具体数据结构已经可用)
+     * @cond    - 指向一个要通知的条件变量
+     *
+     * 备注 : 显然，本函数应该在改变了条件状态后调用;
+     *        说是至少通知一个，实际可能跟系统实现有关
+     */
+
+    /* API  : int pthread_cond_broadcast(pthread_cond_t *cond)
+     * 描述 : 通知所有等待该条件的线程条件已经满足(即条件变量作用的具体数据结构已经可用)
+     * @cond    - 指向一个要广播通知的条件变量
+     */
