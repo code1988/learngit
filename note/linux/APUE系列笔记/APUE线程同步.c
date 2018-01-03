519a4a0a8a4f3604752d7a17e47aa41002a16dff
code <504136046@qq.com>
2018-01-03 23:55:58 +0800

    更新笔记

--- note/linux/APUE系列笔记/APUE线程同步.c
+++ note/linux/APUE系列笔记/APUE线程同步.c
@@ -12,6 +12,9 @@
     /* API  : int pthread_mutex_destroy(pthread_mutex_t mutex)
      * 描述 : 销毁互斥量
      * @mutex   - 指向一个要销毁的互斥量
+     *
+     * 备注 : 销毁之前必须确保没有线程在该互斥量上阻塞
+     *        不需要销毁使用PTHREAD_MUTEX_INITIALIZER初始化的静态分配的互斥量
      */
 
     互斥量主要包含以下3个属性:
@@ -20,5 +23,24 @@
         [3]. 类型属性，linux中缺省值是PTHREAD_MUTEX_NORMAL，意味着标准互斥量类型，不做任何特殊的错误检查或死锁检测
     备注：本人实际使用中通常不会去使用其非默认属性，所以这里不对互斥量非默认属性的设置加以展开，后面的其他同步方式同理
 
+    /* API  : int pthread_mutex_lock(pthread_mutex_t *mutex)
+     * 描述 : 对互斥量进行加锁，如果互斥量已经被其他线程上锁，调用线程将会阻塞直到该互斥量被解锁
+     * @mutex   - 指向一个要加锁的互斥量
+     *
+     * 备注 : 已经对该互斥量上锁的线程，在解锁之前，绝对不允许再次进行加锁，因为直接就会导致死锁
+     */
 
+    /* API  : int pthread_mutex_trylock(pthread_mutex_t *mutex)
+     * 描述 : 对互斥量进行加锁(非阻塞方式)
+     * @mutex   - 指向一个要加锁的互斥量
+     *
+     * 备注 : 如果互斥量已经被其他线程上锁，直接返回 EBUSY
+     */
+
+    /* API  : int pthread_mutex_unlock(pthread_mutex_t *mutex)
+     * 描述 : 对互斥量进行解锁
+     * @mutex   - 指向一个要解锁的互斥量
+     *
+     * 备注 : 如果互斥量已经被其他线程上锁，直接返回 EBUSY
+     */
 
